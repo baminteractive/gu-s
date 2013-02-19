3414ac6c1251d24551cd0dd3b3da59d2469da57f
Ryan Billingsley <ryanbbillingsley@gmail.com>
2013-02-19 15:51:39 -0500

    Moved ip validation to outer scope to stop requests from hanging closes #1

--- routes/index.js
+++ routes/index.js
@@ -12,43 +12,40 @@ exports.index = function(req, res){
 	var query = urlParts.query;
 
 	if(query.ip){
-		mongo.collection('countries',function(err,collection){
-			// Validate ip address
-			var regex = /^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$/;
-			var result = query.ip.match(regex);
-			if(result){
-				ip = query.ip;
-				ipParts = ip.split('.');
+	// Validate ip address
+		var regex = /^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$/;
+		var result = query.ip.match(regex);
+		if(result){
+			mongo.collection('countries',function(err,collection){
+					ip = query.ip;
+					ipParts = ip.split('.');
 
-				collection.find({
-					startFirstOctet : parseInt(ipParts[0]), 
-					startSecondOctet : { $lte : parseInt(ipParts[1]) }, 
-					endSecondOctet : { $gte : parseInt(ipParts[1]) }, 
-					startThirdOctet : { $lte : parseInt(ipParts[2])}, 
-					endThirdOctet : { $gte : parseInt(ipParts[2])} })
-					.toArray(function(err,items){
-						if(items != null && items[0]){
-							if(process.env.ENV_VARIABLE != "production"){
-								console.log(items[0]);
+					collection.find({
+						startFirstOctet : parseInt(ipParts[0]), 
+						startSecondOctet : { $lte : parseInt(ipParts[1]) }, 
+						endSecondOctet : { $gte : parseInt(ipParts[1]) }, 
+						startThirdOctet : { $lte : parseInt(ipParts[2])}, 
+						endThirdOctet : { $gte : parseInt(ipParts[2])} })
+						.toArray(function(err,items){
+							if(items != null && items[0]){
+								if(process.env.ENV_VARIABLE != "production"){
+									console.log(items[0]);
+								}
+
+								country = items[0].country;
+								countryabbr = items[0].countryAbbr;					
+								res.json({country:country,abbr:countryabbr,ip:ip});
+							}else{
+								console.log("No items could be found for that ip.  [IP:"+ip+"]");
+								res.json({country:country,abbr:countryabbr,ip:ip});
 							}
-
-							country = items[0].country;
-							countryabbr = items[0].countryAbbr;					
-							res.json({country:country,abbr:countryabbr,ip:ip});
-						}else{
-							console.log("No items could be found for that ip.  [IP:"+ip+"]");
-							sendResponse(res,country,countryabbr,ip);
-						}
-				}); 
-			}
-		});
+					}); 
+			});
+		}else{
+			res.json({country:country,abbr:countryabbr,ip:ip})
+		}
 	}
 	else{
-		sendResponse(res,country,countryabbr,"");
+		res.json({country:country,abbr:countryabbr,ip:""});
 	}
-};	
-
-
-function sendResponse(res,country,abbr,ip){
-	res.json({country:country,abbr:abbr,ip:ip});
-}
+};
