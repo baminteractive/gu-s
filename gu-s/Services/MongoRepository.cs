2741eaf68d897bb8dd66e8b0f13e754a03a04ad6
Ryan Billingsley <ryanbbillingsley@gmail.com>
2013-02-21 17:40:36 -0500

    Adding Mongo Repo

---
+++ gu-s/Services/MongoRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using gu_s.Models;
+
+namespace gu_s.Services
+{
+    public class MongoRepository
+    {
+        private MongoCollection<Country> _countryCollection;
+
+        public MongoRepository()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["GusDatabase"].ToString();
+            var client = new MongoClient(connectionString);
+            var server = client.GetServer();
+            var database = server.GetDatabase("geoip");
+           _countryCollection = database.GetCollection<Country>("countries");
+        }
+
+        public IEnumerable<Country> All()
+        {
+            return _countryCollection.FindAll();
+        } 
+
+        public IEnumerable<Country> Search(Expression<Func<Country, bool>> predicate)
+        {
+            return _countryCollection.AsQueryable().Where(predicate).ToList();
+        }
+    }
+}
