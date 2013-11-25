f55f97c9f1329cf437f792d6b854db718e9cd8b5
Ryan Billingsley <ryanbbillingsley@gmail.com>
2013-11-25 16:33:28 -0500

    Added a new method that will get the ip from the request to use for the lookup

--- gu-s/Controllers/CountriesController.cs
+++ gu-s/Controllers/CountriesController.cs
@@ -1,23 +1,40 @@
 ﻿using System.Diagnostics;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 using gu_s.Services;
 
 namespace gu_s.Controllers
 {
-    public class CountriesController : ApiController
+  public class CountriesController : ApiController
+  {
+    public HttpResponseMessage Get()
     {
-        // GET api/countries/5
-        public HttpResponseMessage Get(string ip)
-        {
-            var ipLookup = new IpLookup();
-            var result = ipLookup.LookupIp(ip);
+      var ip = GetClientIp(Request);
 
-            Trace.WriteLine(string.Format("Ip: {0} Matched: {1} Message: {2} Country: {3}",result.Ip,result.Matched.ToString(),result.Message,result.Country));
-
-            return result.Matched ? Request.CreateResponse(HttpStatusCode.OK, result.Country) : Request.CreateResponse(HttpStatusCode.BadRequest, result.Message);
-        }
-
+      return LookupCountry(ip);
     }
+
+    // GET api/countries/5
+    public HttpResponseMessage Get(string ip)
+    {
+      return LookupCountry(ip);
+    }
+
+    private HttpResponseMessage LookupCountry(string ip)
+    {
+      var ipLookup = new IpLookup();
+      var result = ipLookup.LookupIp(ip);
+
+      Trace.WriteLine(string.Format("Ip: {0} Matched: {1} Message: {2} Country: {3}", result.Ip, result.Matched.ToString(), result.Message, result.Country));
+
+      return result.Matched ? Request.CreateResponse(HttpStatusCode.OK, result.Country) : Request.CreateResponse(HttpStatusCode.BadRequest, result.Message);
+    }
+
+    private string GetClientIp(HttpRequestMessage request)
+    {
+      return ((System.Web.HttpContextWrapper)Request.Properties["MS_HttpContext"]).Request.UserHostAddress;
+    }
+  }
 }
